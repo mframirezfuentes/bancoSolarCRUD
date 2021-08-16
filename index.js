0d9e44042de3a82101dde1a8c8d355fc71d56edb
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-08-16 09:54:23 -0400

    index inicializado

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const http= require('http')
+const fs=require('fs')
 const url=require('url')
 
 
 http.createServer(async(req,res)=>{
 
     if(req.url=='/'&&req.method =='GET'){
+        res.setHeader('content-type','text,html')
+        const html=fs.readFileSync('index.html','utf8')
+        res.end(html)
 
     }
 }
